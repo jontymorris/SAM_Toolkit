a3137c1f7261d5377ba29e561f5097c143c6bd03
Sam Redmond <samredmondtech@gmail.com>
2018-09-02 16:44:52 +1200

    version 1.1

--- lookup.py
+++ lookup.py
@@ -15,11 +15,10 @@ class Lookup:
         if "Error" in req.text or len(result)==2:
             return False
         else:
-
             return result[1:-1]
 
 if __name__ == "__main__":
-    argument_help = "Bad Argument:\n\t- Syntax: python " + sys.argv[0] + " [email OR file] [-log (optional - saves to file)]"
+    argument_help = "Bad Argument:\n\t- Syntax: python " + sys.argv[0] + " [email/username OR file] [-log (optional - saves to file)]"
     data = {}
     log_file = ""
 
@@ -35,27 +34,38 @@ if __name__ == "__main__":
 
             # Run file
             with open(sys.argv[1]) as f:
+                print("Now searching...")
                 for email in f:
                     email = email.replace("\n", "").replace(" ", "")
                     if email != "":
                         result = lookup.query_database(email)
                         if result:
                             for item in result:
-                                item_split = item.split(":")
-                                e = item_split[0]; p = item.split(":")[1]
-                                print(e + ":" + p)
-                                # Existing scanned email
-                                if e in data:
-                                    if p not in data[e]:
-                                        data[e].append(p)
-                                # Add new email
+                                # Split username and password
+                                split_success = False
+                                for split in [':', ';']:
+                                    item_split = item.split(split)
+                                    if len(item_split) > 1:
+                                        split_success = True
+                                        break
+                                if split_success:
+                                    e = item_split[0]; p = item_split[1]
+                                    print(e + ":" + p)
+                                    # Existing scanned email
+                                    if e in data:
+                                        if p not in data[e]:
+                                            data[e].append(p)
+                                    # Add new email
+                                    else:
+                                        data[e] = [p]
                                 else:
-                                    data[e] = [p]
+                                    print("Splitting Error: couldn't split username from password")
                             
                             # Log into file
                             if log_file != "":
                                 with open(log_file, 'w') as file:
-                                    file.write(json.dumps(data))
+                                    file.write(json.dumps(data, indent=4, sort_keys=False))
+            print("Done!")
         
         # Individual Query                            
         else:
@@ -65,4 +75,3 @@ if __name__ == "__main__":
 
     else: 
         print(argument_help) # Invalid Arguments
-        
